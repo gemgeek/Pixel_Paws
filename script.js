b16fa53040f24598a087e62a446325dfeb800bc7
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-06-24 15:53:42 +0000

    Add Pixel Paws project

---
+++ script.js
@@ -0,0 +1,69 @@
+class Pet {
+  constructor(id) {
+    this.id = id;
+    this.fedLevel = 100;
+    this.fedElement = document.getElementById(`hunger${id}`);
+    this.messageElement = document.getElementById(`message${id}`);
+    this.displayElement = document.getElementById(`pet${id}`);
+  }
+  
+
+  feed(amount) {
+    this.fedLevel += amount;
+    if (this.fedLevel > 100) this.fedLevel = 100;
+    this.fedElement.innerHTML = this.fedLevel;
+  }
+
+  decreaseFedLevel() {
+    this.fedLevel -= 10;
+    if (this.fedLevel < 0) this.fedLevel = 0;
+    this.fedElement.innerHTML = this.fedLevel;
+    if (this.fedLevel === 0) {
+      this.messageElement.innerHTML = "Pet ran away";
+      this.displayElement.style.opacity = 0.5;
+  }
+  }
+}
+
+let pet1 = new Pet(1);
+
+function feedPet(amount) {
+  pet1.feed(amount);
+}
+
+setInterval(() => {
+  pet1.decreaseFedLevel();
+  pet2.decreaseFedLevel();
+}, 5000);
+
+let selectedPet = null;
+
+// Grab elements
+const pet1Element = document.getElementById("pet1");
+const pet2Element = document.getElementById("pet2");
+
+function selectPet(id) {
+  // Update selectedPet
+  selectedPet = id;
+  
+  // Toggle class
+  pet1Element.classList.remove("selected");
+  pet2Element.classList.remove("selected");
+  if (id === 1) {
+    pet1Element.classList.add("selected");
+  } else {
+    pet2Element.classList.add("selected");
+  }
+}
+
+let pet2 = new Pet(2);
+
+function feedPet(amount) {
+  if (selectedPet === 1) {
+    pet1.feed(amount);
+  } else if (selectedPet === 2) {
+    pet2.feed(amount);
+  }
+}
+
+selectPet(1);
